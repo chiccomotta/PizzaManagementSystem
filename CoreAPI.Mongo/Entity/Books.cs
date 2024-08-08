fc5b51d2dc750feac50409af65efa6d7e630bcd1
cristiano motta <cristiano.motta@hotmail.it>
2024-08-08 15:50:08 +0200

    Commit again

--- CoreAPI.Mongo/Entity/Books.cs
+++ CoreAPI.Mongo/Entity/Books.cs
@@ -1,16 +1,15 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace CoreAPI.Mongo.Entity
+namespace CoreAPI.Mongo.Entity;
+
+public class Books
 {
-    public class Books
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string id { get; set; }
-        public string name { get; set; }
-        public int price { get; set; }
-        public string category { get; set; }
-        public string author { get; set; }
-    }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string id { get; set; }
+    public string name { get; set; }
+    public int price { get; set; }
+    public string category { get; set; }
+    public string author { get; set; }
 }
