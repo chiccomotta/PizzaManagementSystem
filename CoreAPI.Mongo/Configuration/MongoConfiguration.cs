86eaecf4e9a7727e12fe92fe45171bb423cf07b5
Chicco <cristiano.motta@hotmail.it>
2024-09-01 16:29:06 +0200

    Minor changes

--- CoreAPI.Mongo/Configuration/MongoConfiguration.cs
+++ CoreAPI.Mongo/Configuration/MongoConfiguration.cs
@@ -1,10 +1,9 @@
-﻿namespace CoreAPI.Mongo.Configuration
+﻿namespace CoreAPI.Mongo.Configuration;
+
+public class MongoConfiguration
 {
-    public class MongoConfiguration
-    {
-        public string BooksCollectionName { get; set; }
-        public string UsersCollectionName { get; set; }
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
+    public string BooksCollectionName { get; set; }
+    public string UsersCollectionName { get; set; }
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
 }
--- CoreAPI.Mongo/Startup.cs
+++ CoreAPI.Mongo/Startup.cs
@@ -7,51 +7,51 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace CoreAPI.Mongo
+namespace CoreAPI.Mongo;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoreAPI.Mongo", Version = "v1" });
+        });
+
+        services.Configure<MongoConfiguration>(Configuration.GetSection("MongoConnectionSettings"));
+        services.AddScoped<IBookService, BookService>();
+        services.AddScoped<IUserService, UserService>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoreAPI.Mongo v1"));
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoreAPI.Mongo", Version = "v1" });
-            });
-            services.Configure<MongoConfiguration>(Configuration.GetSection("MongoConnectionSettings"));
-            services.AddScoped<IBookService, BookService>();
-            services.AddScoped<IUserService, UserService>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoreAPI.Mongo v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
