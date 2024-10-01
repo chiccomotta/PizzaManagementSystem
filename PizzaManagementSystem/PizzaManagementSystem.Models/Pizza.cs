6f5150b3167cb42f9fd57f18dcc3784432ab2a02
Chicco <cristiano.motta@hotmail.it>
2024-10-01 11:27:00 +0200

    Cleanup code

--- PizzaManagementSystem/PizzaManagementSystem.Models/Pizza.cs
+++ PizzaManagementSystem/PizzaManagementSystem.Models/Pizza.cs
@@ -1,9 +1,8 @@
-﻿namespace PizzaManagementSystem.Models
+﻿namespace PizzaManagementSystem.Models;
+
+public class Pizza
 {
-    public class Pizza
-    {
-        public required int Id { get; set; }
-        public required string Name { get; set; }
-        public required decimal Price { get; set; }
-    }
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
 }
