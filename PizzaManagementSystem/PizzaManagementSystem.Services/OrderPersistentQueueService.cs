e73ecd071b90980dc3c89a73d6939fa6124ead87
Chicco <cristiano.motta@hotmail.it>
2024-10-03 11:48:22 +0200

    Minor changes

--- PizzaManagementSystem/PizzaManagementSystem.Services/OrderPersistentQueueService.cs
+++ PizzaManagementSystem/PizzaManagementSystem.Services/OrderPersistentQueueService.cs
@@ -1,33 +1,34 @@
 ﻿using DiskQueue;
 using Microsoft.Extensions.Logging;
 using PizzaManagementSystem.Models;
+// ReSharper disable StringLiteralTypo
 
 namespace PizzaManagementSystem.Services;
 
 public class OrderPersistentQueueService(ILogger<OrderPersistentQueueService> logger) : IOrderService
 {
-    private IPersistentQueue<Order> Orders = new PersistentQueue<Order>("OrdersQueue");
+    private readonly IPersistentQueue<Order> _orders = new PersistentQueue<Order>("OrdersQueue");
 
     public async Task QueueOrder(Order order)
     {
         await Task.Run(() =>
         {
-            using var session = Orders.OpenSession();
+            using var session = _orders.OpenSession();
             session.Enqueue(order);
             session.Flush();
 
-            logger.LogInformation($"Added order id {order.Id} to Queue. Total price is {order.TotalPrice:C}");
+            logger.LogInformation($"Added order id {order.Id} to Queue. Total order price is {order.TotalPrice:C}");
         });
     }
 
     public int GetPendingOrders()
     {
-        return Orders.EstimatedCountOfItemsInQueue;
+        return _orders.EstimatedCountOfItemsInQueue;
     }
 
     public Task<Order?> GetNextOrder()
     {
-        using var session = Orders.OpenSession();
+        using var session = _orders.OpenSession();
         var order = session.Dequeue();
         session.Flush();
         return Task.FromResult(order);
@@ -35,28 +36,28 @@ public class OrderPersistentQueueService(ILogger<OrderPersistentQueueService> lo
 
     public List<Pizza> MenuPizza =>
     [
-        new Pizza()
+        new()
         {
             Id = 1,
             Name = "Margherita",
             Price = 5.0M
         },
 
-        new Pizza()
+        new()
         {
             Id = 2,
             Name = "Ortolana",
             Price = 6.0M
         },
 
-        new Pizza()
+        new()
         {
             Id = 3,
             Name = "Diavola",
             Price = 6.50M
         },
 
-        new Pizza()
+        new()
         {
             Id = 4,
             Name = "Bufalina",
